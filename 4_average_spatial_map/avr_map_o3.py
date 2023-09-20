6be6da88d031131a234454a499a36b729b8ccade
Beiming Tang <51177339+btang1@users.noreply.github.com>
2023-09-20 18:19:57 -0400

    Add files via upload

---
+++ 4_average_spatial_map/avr_map_o3.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 03:08:18 2021
+
+@author: beimingtao
+"""
+
+import numpy as np
+import os
+from scipy import spatial
+import fnmatch
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.basemap import Basemap
+import codecs
+import datetime
+from datetime import datetime as dt
+plt.rc('ytick', labelsize=20)    #change y label fontsize
+plt.rc('xtick', labelsize=20)    #change y label fontsize
+
+'''
+1) import co data original
+'''
+
+
+dirw='/Users/beiming_tang/Desktop/Visu_NOAA_CMAQ/CODE/0_surface_map/cmaq5p4_develop/'
+pattern = 'Surface_Map_o3.nc'                                                  #change chemical species
+filename = dirw+ pattern
+f = Dataset(filename,'r')
+
+Lat_0  = f.variables['lat'][:]
+Lon_0  = f.variables['lon'][:]
+data_0 = f.variables['o3'][:]                                                  #change chemical species
+
+'''
+2) calculate average map
+'''
+data_sum = data_0[0]
+for i in range(1,len(data_0 )):
+    data_sum += data_0[i]
+data_avr = data_sum/len(data_0)
+
+base = dt(2019,7,17,0,0,0)  
+Time_CMAQ = [base + datetime.timedelta(hours=x) for x in range(len(data_0))]   
+'''
+2) plot the map
+'''                                            
+
+m = Basemap(rsphere=(6378137.00,6356752.3142),projection='lcc', resolution='l',
+            llcrnrlat=21, urcrnrlat = 48,llcrnrlon=-123, urcrnrlon =-62,
+            lat_1= 20,lat_2= 60,lat_0=40 ,lon_0= -96)                          #Lambert Projection
+fig = plt.subplots(1,1,figsize = (15,12))     
+m.pcolormesh(Lon_0, Lat_0, data_avr,latlon=True, vmin = 10, vmax = 90, cmap = 'jet')  #change chemical species range
+
+cb =plt.colorbar(orientation="horizontal")
+cb.set_label('$O_3$ (ppbv)',fontsize = 30)                                        #change chemical species                                                          #change units
+cb.ax.tick_params(labelsize=30)
+
+m.drawcoastlines(linewidth=1)
+m.drawcountries()
+m.drawstates()
+plt.title('Averages for $O_3$ from '+ str(Time_CMAQ[0])+'\nto '+ str(Time_CMAQ[-1]),fontsize=30) #change chemical species
+plt.savefig('avr_o3',dpi = 100)                                               #change chemical species
+   
+                                          
+
+
+
+
